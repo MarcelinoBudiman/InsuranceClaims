f711572c64e91ee745c7a2fe4273f3f00001146b
MarcelinoBudiman <BATM.MARCELI@xl.co.id>
2025-04-14 21:25:13 +0700

    Create claim list view

---
+++ InsuranceClaims/Presentation/Home/View/ClaimListView.swift
@@ -0,0 +1,18 @@
+//
+//  ClaimListView.swift
+//  InsuranceClaims
+//
+//  Created by Marcelino Budiman on 14/04/25.
+//
+
+import SwiftUI
+
+struct ClaimListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClaimListView()
+}
