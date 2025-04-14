9a788acdaf0509d0117e8b5795c1a2ab21f9adea
MarcelinoBudiman <BATM.MARCELI@xl.co.id>
2025-04-14 21:17:29 +0700

    Initialize project

---
+++ InsuranceClaims/InsuranceClaimsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsuranceClaimsApp.swift
+//  InsuranceClaims
+//
+//  Created by Marcelino Budiman on 14/04/25.
+//
+
+import SwiftUI
+
+@main
+struct InsuranceClaimsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
