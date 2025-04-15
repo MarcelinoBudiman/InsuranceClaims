690577a33d5e32ff825d29533f3f5af306bbe2f5
MarcelinoBudiman <BATM.MARCELI@xl.co.id>
2025-04-15 12:27:40 +0700

    Integrate API call to claim list view model, Create function to get all claim list

--- InsuranceClaims/Presentation/Home/ViewModel/ClaimListViewModel.swift
+++ InsuranceClaims/Presentation/Home/ViewModel/ClaimListViewModel.swift
@@ -9,8 +9,33 @@ import SwiftUI
 import Foundation
 
 class ClaimListViewModel: ObservableObject {
+    
+    let claimListNetworkClient = NetworkSessionClient<ClaimListEndpoint>()
+    
     @Published var claims: [ClaimPost] = [ClaimPost(userID: 1, id: 1, title: "Claim 001", body: "Claim for dental operation"),
                                           ClaimPost(userID: 1, id: 2, title: "Claim 005", body: "Claim for dental operation"),
                                           ClaimPost(userID: 2, id: 1, title: "Claim 0046", body: "Claim for dental operation"),
                                           ClaimPost(userID: 2, id: 2, title: "Claim 0098", body: "Claim for dental operation")]
+    
+    @Published var claimList: [ClaimPost] = []
+    @Published var errorMessage: String = ""
+    @Published var isLoading: Bool = false
+    
+    @MainActor
+    func getAllClaimList() async {
+        self.isLoading = true
+        
+        let result = await claimListNetworkClient.sendRequest(endpoint: .getAllList, responseModel: [ClaimPost].self)
+        switch result {
+        case .success(let data):
+            self.isLoading = false
+            claimList.removeAll()
+            claimList.append(contentsOf: data)
+            
+        case .failure(let failure):
+            self.isLoading = false
+            errorMessage = failure.rawValue
+        }
+    }
+    
 }
